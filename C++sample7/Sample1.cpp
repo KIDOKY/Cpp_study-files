880cf4b57761f8a03ef68082e2f36a28dbf2d82e
KIDOKY <118824913+KIDOKY@users.noreply.github.com>
2025-03-15 01:04:57 +0900

    Add files via upload

---
+++ C++sample7/Sample1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void buy() {
+    cout << "차를 구입했습니다.\n";
+}
+
+int main() {
+    buy();
+
+    return 0;
+}
---
+++ C++sample7/Sample9.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// max 함수 정의
+inline int max(int x, int y) {
+    if(x >y)
+        return x;
+    else
+        return y;
+}
+
+int main() {
+    int num1, num2, ans;
+
+    cout << "첫 번째 정수를 입력하세요.\n";
+    cin >> num1;
+
+    cout << "두 번째 정수를 입력하세요.\n";
+    cin >> num2;
+
+    ans = max(num1, num2);
+
+    cout << "최댓값은 " << ans << "입니다.\n";
+
+    return 0;
+}
